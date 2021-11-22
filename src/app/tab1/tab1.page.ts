13427062b2ebee31ba5f7be0c99441d8d8b8027c
Believeint <elias.carvalho.096@gmail.com>
2021-11-22 09:22:43 -0300

    Adicionado o component de alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirmar!',
+      message: 'Deseja favoritar <strong>filme</strong>!!!',
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah) => {
+          console.log('Cancelou confirmação: blah' + console.log(this.alertController));
+        }
+      }, {
+        text: 'Sim',
+        handler: () => {
+          console.log('Confirmou');
+        }
+      }],
+    });
+
+    await alert.present();
+  }
 
 }
