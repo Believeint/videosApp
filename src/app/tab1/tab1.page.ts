b438a03bab6972fa40b6c4bf9d629644e8dc4c15
Believeint <elias.carvalho.096@gmail.com>
2021-11-22 12:10:38 -0300

    Adicionado classe de serviço temporário & envio de dados do filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { DadosService } from './../services/dados.service';
 import { IFilmes } from './../models/IFilmes.module';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
@@ -17,25 +19,38 @@ export class Tab1Page {
       lancamento: '15/04/2021',
       duracao: '1h 50m',
       classificacao: 76,
-      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/44aCR6cjH0FbzW6PMw3Ega178iW.jpg',
-      generos: ['Ação', 'Fantasia', 'Aventura']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/44aCR6cjH0FbzW6PMw3Ega178iW.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat',
     },
     {
       nome: 'Liga da Justiça de Zack Snyder (2021)',
       lancamento: '18/03/2021',
       duracao: '4h 2m',
       classificacao: 76,
-      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
-      generos: ['Ação', 'Fantasia', 'Aventura', 'Ficção científica']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura', 'Ficção científica'],
+      pagina: '/liga-justica',
     },
   ];
 
   constructor(
     public alertController: AlertController,
-    public toastController: ToastController
+    public toastController: ToastController,
+    public dadosService: DadosService,
+    public route: Router
   ) {}
 
-  async exibirAlertaFavoritar() {
+  exibirFilme(filme: IFilmes)
+  {
+    this.dadosService.guardarDados('filme', filme);
+    this.route.navigateByUrl('/dados-filme');
+  }
+
+  async exibirAlertaFavoritar(): Promise<void>
+  {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Confirmar!',
